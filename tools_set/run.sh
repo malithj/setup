f4348a489e64ca61bbb44ed61113e075730b2c8c
Malith Jayaweera <malithjayaweera.d@northeastern.edu>
2020-04-08 14:32:06 -0400

    added mercurial tool install

---
+++ tools_set/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# build tools required
+
+# configure install location
+ROOT_DIR="/tmp/test"
+
+# number of jobs for make
+JOBS=4
+
+# configure versions
+MERCURIAL_VERSION=5.3
+
+# navigate to directory
+cd ${ROOT_DIR}
+
+# download llvm-project and checkout tag
+wget https://www.mercurial-scm.org/release/mercurial-${MERCURIAL_VERSION}.tar.gz
+tar -zxvf mercurial-${MERCURIAL_VERSION}.tar.gz
+cd mercurial-${MERCURIAL_VERSION}
+
+# build llvm and clang
+make local
+
+# configure paths
+echo "export HG_PATH=\"${ROOT_DIR}/mercurial-${MERCURIAL_VERSION}\"" >> ~/.bashrc.tmp
+echo "export PATH=\$HG_PATH:\$PATH" >> ~/.bashrc.tmp
+
+# print message on success
+source ~/.bashrc.tmp
+echo "successfully built MERCURIAL version {MERCURIAL_VERSION}"
